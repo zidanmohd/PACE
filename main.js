1ba3ec526da3529b2e1a98d8e656c0b7efebee9e
zidanmohd <78477348+zidanmohd@users.noreply.github.com>
2021-02-04 15:26:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+const usersInput = document.querySelector(".users-input");
+const usersButton = document.querySelector(".users-button");
+const usersList = document.querySelector(".users-list");
+
+usersInput.addEventListener("click", Print);
+let users=usersInput;
+function Print()
+{
+    console.log(users-input)
+}
+Print();
